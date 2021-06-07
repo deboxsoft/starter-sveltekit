6d546566c1330da193a307db07a0c1e6e8b80675
Nurdiansyah <nur.diansyah.ckt@gmail.com>
2021-06-07 10:38:36 +0700

    initial project

---
+++ packages/webapp/src/routes/__layout.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  import Navbar from "__@components/navbars/Navbar.svelte";
+</script>
+<div class="flex flex-col min-h-screen">
+  <Navbar >
+    navbar
+  </Navbar>
+  <!-- page content -->
+  <div class="flex">
+    <div class="card">
+      <slot />
+    </div>
+  </div>
+
+  <!--  footer -->
+  <footer class="flex">
+    <div />
+  </footer>
+</div>
